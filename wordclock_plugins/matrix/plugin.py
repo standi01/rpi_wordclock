1f59f7eb861aafcfd2dacc8b140d9850a2842e43
standi01 <marksonapc@hotmail.co.uk>
2017-06-24 10:50:13 +0100

    Add files via upload

--- wordclock_plugins/matrix/plugin.py
+++ wordclock_plugins/matrix/plugin.py
@@ -1,12 +1,21 @@
 # Authored by Markus E.
 
+import datetime
 import os
+import wordclock_plugins.time_default.time_german as time_german
+import wordclock_plugins.time_default.time_dutch as time_dutch
+import wordclock_plugins.time_default.time_swiss_german as time_swiss_german
+import wordclock_plugins.time_default.time_english as time_english
 import wordclock_tools.wordclock_colors as wcc
 import random
+from ConfigParser import NoSectionError
 
 class plugin:
     '''
-    A class to display matrix rain
+    A class to display the current time (default mode).
+    This default mode needs to be adapted to the hardware
+    layout of the wordclock (the choosen stencil) and is
+    the most essential time display mode of the wordclock.
     '''
 
     def __init__(self, config):
@@ -16,7 +25,27 @@ class plugin:
         # Get plugin name (according to the folder, it is contained in)
         self.name = os.path.dirname(__file__).split('/')[-1]
 
-        self.bg_color = wcc.BLACK;
+        # Choose language
+        try:
+            language = config.get('plugin_' + self.name, 'language')
+        except NoSectionError:
+            language = ''
+        if language == 'german':
+            self.taw = time_german.time_german()
+        elif language == 'dutch':
+            self.taw = time_dutch.time_dutch()
+        elif language == 'swiss_german':
+            self.taw = time_swiss_german.time_swiss_german()
+        elif language == 'english':
+            self.taw = time_english.time_english()
+        else:
+            print('Could not detect language: ' + language + '.')
+            print('Choosing default: german')
+            self.taw = time_german.time_german()
+
+        self.bg_color     = wcc.BLACK  # default background color
+        self.word_color   = wcc.WHITE # default word color
+        self.minute_color = wcc.WHITE # default minute color
 
         self.threshold = 0.9
 
@@ -30,13 +59,20 @@ class plugin:
 
     def run(self, wcd, wci):
         '''
-        Displays rain until aborted by user interaction on pin button_return
+        Displays time until aborted by user interaction on pin button_return
         '''
         # initialize rain start: set to end coordinate
         rain = [20 for _ in range(0, 11)]
         while True:
             # Set background color
             wcd.setColorToAll(self.bg_color, includeMinutes=True)
+            # Set current time
+            now = datetime.datetime.now()
+            # Returns indices, which represent the current time, when beeing illuminated
+            taw_indices = self.taw.get_time(now)
+
+            wcd.setColorBy1DCoordinates(wcd.strip, taw_indices, self.word_color)
+            wcd.setMinutes(now, self.minute_color)
 
             for x,y in enumerate(rain):
                 if (y == 20):
@@ -53,8 +89,9 @@ class plugin:
                         wcd.setColorBy2DCoordinates(wcd.strip, x, yn, color)
                     # advance y coordinate
                     rain[x] = y + 1
+
             wcd.show()
-            # input handling
+
             event = wci.waitSecondsForEvent([wci.button_return, wci.button_left, wci.button_right], 0.1)
             if event == wci.button_return:
                 return
